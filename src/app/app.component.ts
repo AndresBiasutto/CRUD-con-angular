77106f74065ad723840aaf5c6dd547dfb44e491c
Andres Biasutto <aquiandresbiasutto@gmail.com>
2022-04-11 01:16:21 -0300

    primerisima super verde verdesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Empleado } from './modelos/empleados';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+  title = 'Crud simple en angular';
+  ArrayDeEmpleados: Empleado[] = [
+    {id: 1, nombre: "Cachito McSheep", pais: "Escocia" },
+    {id: 2, nombre: "Pepe Buonaroppa", pais: "Italia" },
+    {id: 3, nombre: "Tincho Gomez", pais: "Argentina" }
+  ];
+  //de la siguiente forma, el template llama a esta variable, creando un nuevo objeto  Empleado
+   empleadoSeleccionado: Empleado = new Empleado();
+
+   agregarOeditar(){
+     if(this.empleadoSeleccionado.id === 0){
+       this.empleadoSeleccionado.id = this.ArrayDeEmpleados.length + 1;
+     this.ArrayDeEmpleados.push(this.empleadoSeleccionado);
+     }
+     
+     this.empleadoSeleccionado = new  Empleado();
+   }
+   abrirYeditar(empleado: Empleado){
+    this.empleadoSeleccionado = empleado;
+   }
+   borrar(empleadoSeleccionado: Empleado){
+     this.ArrayDeEmpleados = this.ArrayDeEmpleados.filter(x => x != this.empleadoSeleccionado)
+    this.empleadoSeleccionado = new Empleado;
+    }
 }
